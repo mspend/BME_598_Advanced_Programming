0605776b97de8bff30dcf2e001b5243edaeb8cbb
mspend <mauridobbs@gmail.com>
2025-09-24 23:44:26 -0700

    added 2 more violin plot comparisons

--- module_5/homework_5.py
+++ module_5/homework_5.py
@@ -1,3 +1,6 @@
+# Mauri Spendlove
+# 
+
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
@@ -32,6 +35,11 @@ print(panCancer_phenos['tumor'].unique())
 # How many counts are there of each tumor type, descending?
 print(panCancer_phenos.value_counts(subset='tumor'))
 
+# Drop the columns we don't need to keep the most data possible
+panCancer_phenos = panCancer_phenos.drop(columns = 'subtype')
+panCancer_phenos = panCancer_phenos.drop(columns = 'OS')
+
+
 # Cancers of interest: 
 # 1. bladder urothelial carcinoma = BLCA
 # 2. lung adenocarcinoma = LUAD
@@ -39,28 +47,72 @@ print(panCancer_phenos.value_counts(subset='tumor'))
 
 cancers_of_interest = panCancer_phenos.loc[(panCancer_phenos.tumor=='BLCA') | (panCancer_phenos.tumor=='LUAD') | (panCancer_phenos.tumor=='SKCM'),:]
 
-# Drop the columns we don't need to keep the most data possible
-cancers_of_interest = cancers_of_interest.drop(columns = 'subtype')
-cancers_of_interest = cancers_of_interest.drop(columns = 'OS')
-
 # Drop missing values
 cancers_of_interest = cancers_of_interest.dropna()
 
+##### Plot #1: Tumor Type vs Age
 # Select only cancers of interest
 cancers_of_interest = ["BLCA", "LUAD", "SKCM"]
 subset = panCancer_phenos[panCancer_phenos["tumor"].isin(cancers_of_interest)]
 
-# Prepare data for violinplot
-data = [subset[subset["tumor"] == g]["age_at_initial_pathologic_diagnosis"].dropna()
+# Prepare data for age violinplot
+data_1 = [subset[subset["tumor"] == g]["age_at_initial_pathologic_diagnosis"].dropna()
         for g in cancers_of_interest]
 
-# Create violin plot
+# Create violin plot of age vs tumor type
 plt.figure(figsize=(6,5))
-plt.violinplot(data, showmeans=True)
+plt.violinplot(data_1, showmeans=True)
 
 # Label axes
 plt.xticks(np.arange(1,len(cancers_of_interest)+1), cancers_of_interest)
 plt.xlabel("Tumor Type")
 plt.ylabel("Age at Initial Diagnosis (Years)")
 plt.title("Examining Age at Initial Diagnosis")
+plt.show()
+
+##### Plot #2: Sex vs Survival
+# Select data
+sexes = ["MALE", "FEMALE"]
+subset_2 = panCancer_phenos[panCancer_phenos["gender"].isin(sexes)]
+
+# Prepare data for gender violinplot
+data_2 = [panCancer_phenos[panCancer_phenos["gender"] == g]["OS.time"].dropna()
+        for g in sexes]
+
+# Create violin plot of gender vs survival time
+plt.figure(figsize=(6,5))
+plt.violinplot(data_2, showmeans=True)
+
+# Label axes
+plt.xticks(np.arange(1,len(sexes)+1), sexes)
+plt.xlabel("Sex")
+plt.ylabel("Survival (Days)")
+plt.title("Relationship between Sex and Overall Survival time")
+plt.show()
+
+# Print median survival time for both men and women
+median_survival = panCancer_phenos.groupby('gender')['OS.time'].median()
+print(median_survival)
+
+##### Plot #3: Race vs Age
+# Select data
+races = ['WHITE', 'BLACK OR AFRICAN AMERICAN', 'ASIAN', 
+         'AMERICAN INDIAN OR ALASKA NATIVE', 'NATIVE HAWAIIAN OR OTHER PACIFIC ISLANDER', '[Unknown]']
+subset_3 = panCancer_phenos[panCancer_phenos["race"].isin(races)]
+
+# Prepare data: group age values by race
+data_3 = [
+    subset_3[subset_3["race"] == race]["age_at_initial_pathologic_diagnosis"].dropna()
+    for race in races
+]
+
+# Create violin plot of age vs race
+plt.figure(figsize=(6,5))
+plt.violinplot(data_3, vert=False, showmeans=True)
+
+# Label axes
+plt.yticks(np.arange(1,len(races)+1), races)
+plt.ylabel("Race")
+plt.xlabel("Age at Initial Diagnosis (Years)")
+plt.title("Relationship Between Race and Age at Initial Diagnosis")
 plt.show()
