3a2eb109c6c352935e41e53a9f7ce78576df518c
mspend <mauridobbs@gmail.com>
2025-10-01 23:52:21 -0700

    changed file name

--- homework_6.py
+++ homework_6.py
@@ -185,7 +185,7 @@ with PdfPages('violinPlots_byCellLine_byPerturbation.pdf') as pdf:
 #     f. Convert the dictionary into a pandas DataFrame called 'perturbation_comparisons_df'
 #     g. Write out the DataFrame to a csv file called 'perturbation_comparisons_df.csv'
 
-perturbation_comparisons = pd.DataFrame(columns= ['HA.FC', 'HA.stat', 'HA.pvalue', 'T98G.FC', 'T98G.stat', 'T98G.pvalue', 'U251.FC', 'U251.stat', 'U251.pvalue'])
+perturbation_comparisons_df = pd.DataFrame(columns= ['HA.FC', 'HA.stat', 'HA.pvalue', 'T98G.FC', 'T98G.stat', 'T98G.pvalue', 'U251.FC', 'U251.stat', 'U251.pvalue'])
 
 perturbation_comparisons.to_csv('perturbation_comparisons_df.csv')
 
