7ad52af79d88e402f6452708fe6e580c39ecbc80
mspend <mauridobbs@gmail.com>
2025-10-07 20:17:36 -0700

    calculate correlations between quantitative variables in stroke dataset

--- module_7/homework_7.py
+++ module_7/homework_7.py
@@ -30,8 +30,6 @@
 #             B. The overview of analytical steps should be written as a outline with bullet points. Provide more detail than the headings of the Deliverables section.
 
 
-
-
 ## 2. (10pts) Load up the packages you will need to run
 #  Deliverables:
 #     a. Restart console: Consoles tab -> Restart Kernel
@@ -44,7 +42,6 @@ import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
 
 
-
 ## 3. (10pts) Load up and inspect data and function
 #  Deliverables:
 #     a. Use a text editor to determine the structure of the 'Raw_data_TRP_with_ischemic_stroke.csv' file
@@ -53,26 +50,20 @@ from matplotlib.backends.backend_pdf import PdfPages
 #        - Index column?
 #     c. Read the file using pandas into a variable named 'stroke'
 #     d. Make sure the shape of the file is correct
-#        - Expectation is (462 rows, by 10 columns)
+#           Expectation is (519 rows, by 15 columns)
 stroke = pd.read_csv('Raw_data_TRP_with_ischemic_stroke.csv', delimiter=',', header=0)
 print(stroke.shape) 
-
-
 
 ## 4. (10pts) Make a pairs plot of your data to identify which variables to test in correlation studies.
 #  Deliverables:
 #     a. Write a pairsplot PDF using PdfPages to a pdf called 'stroke_data_pairplot.pdf'
 with PdfPages('stroke_data_pairplot.pdf') as pdf:
-    # Pairs plot of quantitative data
     plt.figure(figsize=(6, 6))
     sns.pairplot(data=stroke)
     plt.title('Title')
     plt.tight_layout()
     pdf.savefig()
     plt.close()
-
-
-
 
 ## 5. (20pts) Compute pairwise correlations
 #  Deliverables:
@@ -84,7 +75,24 @@ with PdfPages('stroke_data_pairplot.pdf') as pdf:
 #     c. Fill the dictionaries up with the values from the pairwise correlation comparisons of the 'quant_variables'
 #        - NOTE: set the comparison of a variable to itself as 0
 
+# Notice that the data has NA values. This will give an error when we run correlations later.
+print(stroke.info())
+stroke = stroke.dropna()
 
+vars_as_string = "Age,SBP,DBP,HbA1c,FBS,TC,LDL,HDL,TG,WBC,CRP,AIP,TyG"
+
+quant_variables = vars_as_string.split(sep=',')
+pearson_cor = {}
+pearson_pv = {}
+
+for q1 in quant_variables:
+    pearson_cor[q1] = {}
+    pearson_pv[q1] = {}
+    for q2 in quant_variables:
+        if not q1==q2:
+            pearson_cor[q1][q2], pearson_pv[q1][q2] = pearsonr(stroke[q1], stroke[q2])
+        else:
+            pearson_cor[q1][q2], pearson_pv[q1][q2] = (0, np.nan)
 
 
 ## 6. (10pts) Convert result dictionaries into pd.DataFrames and write them out as CSV files
@@ -96,8 +104,23 @@ with PdfPages('stroke_data_pairplot.pdf') as pdf:
 #        and p-value '.pv'
 #     c. Then write out the merged pd.DataFrame as 'stroke_pearson_correlation.csv'
 
+# Convert to pandas DataFrames
+pearson_cor_df = pd.DataFrame(pearson_cor)
+pearson_pv_df = pd.DataFrame(pearson_pv)
 
+# Confirm the conversion to DataFrame worked
+print(type(pearson_cor_df))
 
+# Write out combined R and PV matrix
+# axis = 1 means concatenate along the columns (axis=0 is for index)
+combined_df = pd.concat([pearson_cor_df, pearson_pv_df], axis=1).sort_index(axis=1)
+
+# add labels to column headers to make the combined_df easier to read
+# Otherwise there would be 2 columns with the same name for each variable
+# .pv for P-value if the column numbers (indexed from 1) are even, .R for Pearson R
+# When the dfs were concatenated, the df listed first (pearson_cor_df) was on the left, before sorting.
+combined_df.columns = [combined_df.columns[i]+'.pv' if ((i+1)%2)==0 else combined_df.columns[i]+'.R' for i in range(len(combined_df.columns))]
+combined_df.to_csv('stroke_pearson_correlation.csv')
 
 ## 7. (10pts) Visiualize the correlation matrix as a heatmap
 #  Deliverables:
@@ -108,8 +131,13 @@ with PdfPages('stroke_data_pairplot.pdf') as pdf:
 #     c. Maximum and minimum correlation values should be set at 1 and -1, respectively
 #     d. Must include units for the colorbar legend
 
-
-
+# Make a plot of the relationships
+with PdfPages('stroke_pearson_cor_df_heatmap.pdf') as pdf:
+    plt.figure(figsize=(6, 6))
+    hm1 = sns.heatmap(data=pearson_cor_df, cmap=sns.color_palette("vlag", as_cmap=True), vmin=-1, vmax=1, cbar_kws={'label': 'Pearson\'s R'})
+    plt.tight_layout()
+    pdf.savefig()
+    plt.close()
 
 ## 8. (20pts) Biological interpretation that is to be completed in a separate Word document that will be converted into a PDF for submission
 #  Deliverables:
