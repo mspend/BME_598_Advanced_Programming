437bde0633a44f4f359d7c7085f93143e3bd5c95
mspend <mauridobbs@gmail.com>
2025-10-28 10:08:37 -0700

    plot continuous variables

--- module_10/in_class_activity_10_2.py
+++ module_10/in_class_activity_10_2.py
@@ -18,3 +18,26 @@ import pandas as pd
 import statsmodels.formula.api as smf
 
 fev = pd.read_table(filepath_or_buffer='fev_dat.txt', delimiter='\t')
+
+fev_continuous_vars = fev.drop(columns=['sex','smoke'])
+
+with PdfPages('scatterPlots_fev_data.pdf') as savedPDF:
+    # Pairplot
+    sns.pairplot(fev_continuous_vars)
+    savedPDF.savefig()
+    plt.close()
+
+
+
+
+
+
+
+
+
+
+
+
+# look for an interaction between age and smoking
+# Question 7
+smf.ols('FEV~age*smoke',data=fev)
