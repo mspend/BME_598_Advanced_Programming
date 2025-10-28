b8d2f70c77cc76b1ab625dab3b9c5eee366843cd
mspend <mauridobbs@gmail.com>
2025-10-28 09:54:47 -0700

    added files for the lecture 10.2

---
+++ module_10/in_class_activity_10_2.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 28 09:33:10 2025
+
+@author: maurispendlove
+"""
+
+import matplotlib
+# matplotlib.use('Agg')
+# matplotlib.rcParams['pdf.fonttype'] = 42
+from matplotlib.backends.backend_pdf import PdfPages
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+import numpy as np
+import pandas as pd
+import statsmodels.formula.api as smf
+
