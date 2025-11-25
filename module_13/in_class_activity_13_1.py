56bcf9752d2a177749298fa1a6d2b85ef1cf80ec
mspend <mauridobbs@gmail.com>
2025-11-25 09:49:00 -0700

    added a new module - module 13

---
+++ module_13/in_class_activity_13_1.py
@@ -0,0 +1,52 @@
+##########################################################
+## OncoMerge:  in_class_activity_13_1.py                ##
+##  ______     ______     __  __                        ##
+## /\  __ \   /\  ___\   /\ \/\ \                       ##
+## \ \  __ \  \ \___  \  \ \ \_\ \                      ##
+##  \ \_\ \_\  \/\_____\  \ \_____\                     ##
+##   \/_/\/_/   \/_____/   \/_____/                     ##
+## @Developed by: Plaisier Lab                          ##
+##   (https://plaisierlab.engineering.asu.edu/)         ##
+##   Arizona State University                           ##
+##   242 ISTB1, 550 E Orange St                         ##
+##   Tempe, AZ  85281                                   ##
+## @Author:  Chris Plaisier                             ##
+## @License:  GNU GPLv3                                 ##
+##                                                      ##
+## If this program is used in your analysis please      ##
+## mention who built it. Thanks. :-)                    ##
+##########################################################
+
+
+## May need to install
+# pip install pomegranate==0.15.0
+
+
+## 1. Load libraries
+from pomegranate import *
+
+
+## 2. Markov chains
+
+# Define starting probabilities
+start = DiscreteDistribution({
+    "sun": 0.5,
+    "rain": 0.5
+})
+
+# Define transition model
+transitions = ConditionalProbabilityTable([
+    ["sun", "sun", 0.8],
+    ["sun", "rain", 0.2],
+    ["rain", "sun", 0.3],
+    ["rain", "rain", 0.7]
+], [start])
+
+
+## 3. Create Markov chain
+model = MarkovChain([start, transitions])
+
+
+## 4. Sample 50 states from chain
+print(model.sample(50))
+
