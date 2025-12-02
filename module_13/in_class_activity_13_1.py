0abcd50760e3a196ededef1a32f2c462f9368e02
mspend <mauridobbs@gmail.com>
2025-12-02 09:14:00 -0700

    updated code for markov chains

--- module_13/in_class_activity_13_1.py
+++ module_13/in_class_activity_13_1.py
@@ -18,35 +18,46 @@
 ##########################################################
 
 
-## May need to install
-# pip install pomegranate==0.15.0
-
-
 ## 1. Load libraries
-from pomegranate import *
+import numpy as np
 
 
-## 2. Markov chains
-
-# Define starting probabilities
-start = DiscreteDistribution({
-    "sun": 0.5,
-    "rain": 0.5
-})
-
-# Define transition model
-transitions = ConditionalProbabilityTable([
-    ["sun", "sun", 0.8],
-    ["sun", "rain", 0.2],
-    ["rain", "sun", 0.3],
-    ["rain", "rain", 0.7]
-], [start])
+## 2. Define starting probabilities
+start = { "sun": 0.5, "rain": 0.5 }
 
 
-## 3. Create Markov chain
-model = MarkovChain([start, transitions])
+## 3. Define transition probabilities
+transitions = {"sun": { "sun": 0.95, "rain": 0.05},
+               "rain": {"sun": 0.8, "rain": 0.2}}
 
 
-## 4. Sample 50 states from chain
+## 4. MarkovChain function to predict states
+def MarkovChain(start, transitions, num_states):
+    # State chain
+    states = []
+        
+    # Ensure the probabilities sum to 1 (a good practice check)
+    if not np.isclose(sum(start.values()), 1.0):
+        raise ValueError("Start likelihoods must sum to approximately 1.0")
+    
+    # First, choose a starting state using 
+    states.append(np.random.choice(list(start.keys()), size=1, p=list(start.values()))[0])
+    
+    # Next add other states
+    for i in range(num_states-1):
+        states.append(np.random.choice(list(transitions[states[-1]].keys()), size=1, p=list(transitions[states[-1]].values()))[0])
+    
+    # Return states
+    return(states)
+
+
+## 5. Compute 50 samples
+result = MarkovChain(start, transitions, 365)
+print(result)
+print('sun = '+str(np.sum([i=='sun' for i in result])))
+print('rain = '+str(np.sum([i=='rain' for i in result])))
+
+
+## 6. Sample 50 states from chain
 print(model.sample(50))
 
